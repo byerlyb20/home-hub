b0c0e1df40b07aa4035b8d0bdac57ba6a516c606
Brigham Byerly <6891883+byerlyb20@users.noreply.github.com>
2023-12-15 20:52:37 -0500

    Fixed index page

--- web/index.js
+++ web/index.js
@@ -1,10 +1,12 @@
-async function updateUserDetail() {
-    const userInfo = await user()
-    if (userInfo !== undefined) {
-        document.getElementById('activeSessionUsernameDetail').innerText = userInfo.username
-        document.getElementById('activeSessionUserIDDetail').innerText = userInfo.id
-    }
-}
+import { register, logout, user, createAPIToken } from "./auth.js"
+
+document.addEventListener("DOMContentLoaded", async function(event) {
+    document.getElementById('registerButton').addEventListener('click', async function(event) {
+        let username = document.getElementById('inputUsername').value
+        await register(username)
+    })
+    document.getElementById('tokenButton').addEventListener('click', apiTokenFormGenerate)
+})
 
 async function apiTokenFormGenerate() {
     const friendlyName = document.getElementById('inputAPITokenFriendlyName').value
